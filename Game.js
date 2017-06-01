cd59252507faaa8f3c18f4ff3c818a7fd8ccd043
Lee1997 <leelaffan97@gmail.com>
2017-06-02 00:53:44 +0100

    Added dealing and selecting cards for exchnage functionality

--- Game.js
+++ Game.js
@@ -1,6 +1,5 @@
-function startGame(){
+startGame = function(){
   var deck = makeDeck();
-  alert();
 
   /*for(var i = 0; i < 52; i++){
     console.log(deck[i].getInfo());
@@ -8,10 +7,34 @@ function startGame(){
 
   deck = shuffle(deck);
 
-  var cards = $('.card');
+  var myCards = $('.myCard');
 
-  for(var i = 0; i < cards.length; i++){
-    cards[i].setAttribute('src', deck[i].getImgSrc());
+  for(var i = 0; i < myCards.length; i++){
+    myCards[i].setAttribute('src', deck[i].getImgSrc());
+    myCards[i].setAttribute('onclick', 'selectCard(this)');
   }
+}
 
+function selectCard(card){
+  card.setAttribute("style", "opacity: 0.4");
+  card.setAttribute("onclick", "deselectCard(this)");
+}
+
+function deselectCard(card){
+  card.setAttribute("style", "");
+  card.setAttribute("onclick", "selectCard(this)");
+}
+
+function rand(min, max) {
+  return Math.floor(Math.random()*(max-min)+min);
+}
+
+function exchangeCards(){
+  var myCards = $('.myCard');
+
+  for(var i = 0; i < myCards.length; i++){
+    if(myCards[i].style.cssText != ""){
+      alert(myCards[i].style.cssText);
+    }
+  }
 }
